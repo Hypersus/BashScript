2ed3fbc2594ecdc211cb4e10a43ef1f542e78395
zeke <hypersus@outlook.com>
2022-08-27 20:20:16 +0800

    Initial commit

---
+++ helloworld.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ #===================================================================#
+ #	Copyright (C) 2022 Zeke. All rights reserved
+ # 
+ #	Filename:		helloworld.sh
+ #	Author:			Zeke
+ #	Date:			2022.08.27
+ #	E-mail:			hypersus@outlook.com
+ #	Discription:	test script
+ #	
+ #===================================================================#
+
+echo "Hello World!"
